91f5e382d01d1965d0c3e73adbf4419167bc03f7
Florent Uzio <fuzio@ripple.com>
2024-09-09 18:34:19 +0200

    updated payment example

--- examples/websocket/send-payment/main.go
+++ examples/websocket/send-payment/main.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"github.com/Peersyst/xrpl-go/xrpl"
+	public_servers "github.com/Peersyst/xrpl-go/xrpl/client/public_servers"
 	"github.com/Peersyst/xrpl-go/xrpl/client/websocket"
 	"github.com/Peersyst/xrpl-go/xrpl/faucet"
 	"github.com/Peersyst/xrpl-go/xrpl/model/transactions"
@@ -26,9 +27,12 @@ func main() {
 		return
 	}
 
+	// init public urls for websocket client
+	publicServers := public_servers.NewServerUrls()
+
 	client := websocket.NewWebsocketClient(
 		websocket.NewWebsocketClientConfig().
-			WithHost("wss://s.altnet.rippletest.net:51233").
+			WithHost(publicServers.TestnetWebSocket().Ripple()).
 			WithFaucetProvider(faucet.NewTestnetFaucetProvider()),
 	)
 
