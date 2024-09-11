c8d695ad7b01e73975074c9a8470a3389bc85381
Florent Uzio <fuzio@ripple.com>
2024-09-11 15:10:07 +0400

    validation for account delete

--- xrpl/model/transactions/account_delete.go
+++ xrpl/model/transactions/account_delete.go
@@ -1,6 +1,7 @@
 package transactions
 
 import (
+	"github.com/Peersyst/xrpl-go/pkg/typecheck"
 	"github.com/Peersyst/xrpl-go/xrpl/model/transactions/types"
 )
 
@@ -16,5 +17,20 @@ func (*AccountDelete) TxType() TxType {
 
 // TODO: Implement flatten
 func (s *AccountDelete) Flatten() FlatTransaction {
+	return nil
+}
+
+func ValidateAccountDelete(tx FlatTransaction) error {
+	// Validate base transaction fields
+	err := ValidateBaseTransaction(tx)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Replace IsString by a check for valid xrpl address
+	ValidateRequiredField(tx, "Destination", typecheck.IsString)
+
+	ValidateOptionalField(tx, "DestinationTag", typecheck.IsInt)
+
 	return nil
 }
