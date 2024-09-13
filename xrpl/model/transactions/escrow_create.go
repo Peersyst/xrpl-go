9aab88329c154180a27f0f933f1e864c54877af2
Florent Uzio <fuzio@ripple.com>
2024-09-13 14:30:31 +0400

    escrow create validations

--- xrpl/model/transactions/escrow_create.go
+++ xrpl/model/transactions/escrow_create.go
@@ -1,6 +1,9 @@
 package transactions
 
 import (
+	"errors"
+
+	"github.com/Peersyst/xrpl-go/pkg/typecheck"
 	"github.com/Peersyst/xrpl-go/xrpl/model/transactions/types"
 )
 
@@ -20,5 +23,40 @@ func (*EscrowCreate) TxType() TxType {
 
 // TODO: Implement flatten
 func (s *EscrowCreate) Flatten() FlatTransaction {
+	return nil
+}
+
+func ValidateEscrowCreate(tx FlatTransaction) error {
+	err := ValidateBaseTransaction(tx)
+	if err != nil {
+		return err
+	}
+
+	err = ValidateRequiredField(tx, "Amount", typecheck.IsString)
+	if err != nil {
+		return err
+	}
+
+	// TODO: update to IsAccount when that function exists
+	err = ValidateRequiredField(tx, "Destination", typecheck.IsString)
+	if err != nil {
+		return err
+	}
+
+	err = ValidateOptionalField(tx, "DestinationTag", typecheck.IsUint)
+	if err != nil {
+		return err
+	}
+
+	_, hasCancelAfter := tx["CancelAfter"]
+	_, hasFinishAfter := tx["FinishAfter"]
+
+	if !hasCancelAfter && !hasFinishAfter {
+		return errors.New("EscrowCreate: must provide either CancelAfter or FinishAfter field")
+	}
+
+	ValidateOptionalField(tx, "CancelAfter", typecheck.IsUint)
+	ValidateOptionalField(tx, "FinishAfter", typecheck.IsUint)
+	ValidateOptionalField(tx, "Condition", typecheck.IsString)
 	return nil
 }
--- xrpl/model/transactions/tx.go
+++ xrpl/model/transactions/tx.go
@@ -341,6 +341,11 @@ func ValidateTx(tx FlatTransaction) error {
 		if err != nil {
 			return err
 		}
+	case "EscrowCreate":
+		err = ValidateEscrowCreate(tx)
+		if err != nil {
+			return err
+		}
 	case "Payment":
 		err = ValidatePayment(tx)
 		if err != nil {
